fdd6904bd1db24b81e05dc1bba68e8e944f09849
Marcelo <marcelomilhomemm@hotmail.com>
2022-05-15 17:40:13 +0100

    try axios

--- models/Movie.model.js
+++ models/Movie.model.js
@@ -1,8 +1,9 @@
 const { Schema, model } = require("mongoose");
+const Movie = model("Movie", movieSchema);
 
 
 const movieSchema = new Schema({
     title: String,
 });
-const Movie = model("Movie", movieSchema);
+
 module.exports = Movie;
--- routes/movie.routes.js
+++ routes/movie.routes.js
@@ -2,23 +2,40 @@ const router = require("express").Router();
 const isLoggedOut = require("../middleware/isLoggedOut");
 const isLoggedIn = require("../middleware/isLoggedIn");
 const Movie = require("../models/Movie.model");
+const { default: axios } = require("axios");
+const { response } = require("express");
 
 //isLoggedIn
 
+router.get("movie-search", (req, res, next) => {
+  const { name } = req.query;
+  axios
+    .get(
+      `https://api.themoviedb.org/3/search/movie?api_key=bdb54a5f51e436feb6807b80aebf0c21&language=en-US&query=${name}`
+    )
+    .then((response) => {
+      response.data.results.forEach((element) => {
+        console.log(element.title);
+      });
+      res.render("search-movie", { movieList: response.data.results });
+    })
+    .catch((err) => next(err));
+});
+
 router.get("/movie-random", isLoggedIn, (req, res, next) => {
-    res.render("movies/movie-random");
+  res.render("movies/movie-random");
 });
 
 router.get("/movie-detail", (req, res, next) => {
-    res.render("movies/movie-detail");
+  res.render("movies/movie-detail");
 });
 
 router.get("/movies-list", isLoggedIn, (req, res, next) => {
-    res.render("movies/movies-list");
+  res.render("movies/movies-list");
 });
 
 router.get("/watch-list", isLoggedIn, (req, res, next) => {
-    res.render("movies/watch-list");
+  res.render("movies/watch-list");
 });
 
 module.exports = router;
